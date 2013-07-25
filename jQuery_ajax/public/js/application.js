c58c210a8939ca135f53a14fb0d9819a0cd95caf
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-25 10:25:29 -0500

    done did it

--- jQuery_ajax/public/js/application.js
+++ jQuery_ajax/public/js/application.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+  $("#get_color").click(function(event) {
+    event.preventDefault;
+    $.post('/color', function(response){
+      console.log(response);
+      $("ul li:nth-child(" +response.cell+ ")").css('background-color', response.color);
+
+    });
+  })
 
 });
